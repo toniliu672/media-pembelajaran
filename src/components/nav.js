ff507aebbf1a1cdf02844ac63698c78d9ddd48fd
Toni Liungan <toniliu672@gmail.com>
2023-05-05 14:26:30 +0800

    fix hamburger menu

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,11 +4,18 @@ const sidebar = document.querySelector('.sidebar');
 const line1 = document.querySelector('.line-1');
 const line2 = document.querySelector('.line-2');
 const line3 = document.querySelector('.line-3');
+const sidebarLinks = document.querySelectorAll('.sidebar a');
 
-hamburger.addEventListener('click', () => {
+function toggleSidebar() {
   sidebar.style.left = sidebar.style.left === '0px' ? '-100%' : '0px';
   
   line1.classList.toggle('rotate-line-1');
   line2.classList.toggle('hide-line-2');
   line3.classList.toggle('rotate-line-3');
+}
+
+hamburger.addEventListener('click', toggleSidebar);
+
+sidebarLinks.forEach(link => {
+  link.addEventListener('click', toggleSidebar);
 });
